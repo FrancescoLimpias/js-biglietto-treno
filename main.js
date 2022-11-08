00542981ba83f9783dadb8cef8bafcd6f5826922
Francesco Limpias <francescolimpias@gmail.com>
2022-11-08 16:51:14 +0100

    Conformed to standards

--- main.js
+++ main.js
@@ -1,163 +1,25 @@
-// Start Logging
-console.log("Interact with the RailwayMan trough the global object \'RailMan\'!");
-console.log("Ask for a ticket and be kind! ;)");
-console.log("HINT: you may find RailMan available methods typing \'RailMan.\' in the console and looking at your browser autocompletions")
-addEventListener("TALK", (event) => {
-    console.log("\n" + event.detail + "\n ", "font-size: 15px", "color: beige; font-size: 15px");
-});
+// Get Data
+let km = parseInt(prompt("Quanti km vuoi percorrere?"));
+let age = parseInt(prompt("Quanti anni hai?"));
 
-function autoPlay() {
-    // Sample forms
-    const
-        formYoung = {
-            age: 10,
-            km: 20
-        },
-        formOld = {
-            age: 80,
-            km: 20
-        },
-        formNormal = {
-            age: 40,
-            km: 20
-        };
+if(
+    isNaN(km)
+    || km < 0
+    || isNaN(age)
+    || age < 0
+){
+    alert("Dati non supportati");
+} else {
 
-    // Angry RailMan
-    console.log("Angry RailMan");
-    RailMan = new RailwayMan("Mr.TooSeriousToPlay :D", "BAD");
-    RailMan.askForTicket();
-    RailMan.askForTicket(formYoung);
-    RailMan.askForTicket(formNormal);
-    RailMan.askForTicket(formOld);
-
-    // Happy RailMan
-    console.log("Happy RailMan");
-    RailMan.compliment()
-    RailMan.askForTicket(formYoung);
-    RailMan.askForTicket(formNormal);
-    RailMan.askForTicket(formOld);
-
-    console.log("\nFinished Autoplay\n ");
-
-    // Back to normality
-    RailMan = new RailwayMan("Mike");
+    // Calc pricePerKM
+    let eurosPerKM = 0.21;
+    if(age < 18){
+        eurosPerKM -= eurosPerKM * 0.2;
+    } else if(age > 65){
+        eurosPerKM -= eurosPerKM * 0.4;
+    }
+    
+    // Calc price
+    let price = (km * eurosPerKM).toFixed(2);
+    alert(`Il prezzo del biglietto è di ${price}€`);
 }
-
-// RAILWAY MAN
-class RailwayMan {
-
-    // ID
-    #name;
-    #mood;
-
-    constructor(name, mood) {
-        this.#name = name;
-        this.#mood = mood ?? Math.round(Math.random()) == 0 ? "BAD" : "GOOD";
-
-        this.#say(
-            `Hi! I'm the RailwayMan, 
-            my name is ${this.#name}. 
-            How may I serve you?`
-        );
-    }
-
-    // This is just a fancy "console.log()"
-    #say(sentence /* string */) {
-        sentence = "%c[RailMan] %c" + sentence;
-        const event = new CustomEvent("TALK", {
-            "detail": sentence,
-        })
-        window.dispatchEvent(event);
-    }
-
-    compliment() {
-        this.#say("Oh thanks! I really needed that today!");
-        this.#mood = "GOOD";
-    }
-
-    askForTicket(form) {
-        // Greets player
-        this.#say("Ohh I see, so you need a ticket...");
-
-        if (form) {
-            // Player has form
-            console.log(JSON.stringify(form));
-
-            // Form validation
-            if(!(
-
-                // Age
-                form.age
-                && parseInt(form.age) != NaN
-                && form.age > 0
-
-                // KM
-                && form.km
-                && parseFloat(form.km) != NaN
-                && form.km > 0
-                
-                )){
-                this.#say("Sir, I'm afraid yours is a faulty form, please check it again");
-                return;
-            }
-
-            let eurosPerKm = 0.21; //standard rate
-
-            // Let's apply some discounts
-            if (form.age < 18) {
-                this.#say("Well young man, you have a discount of 20%");
-                eurosPerKm -= eurosPerKm * .20;
-            } else if (form.age > 65) {
-                this.#say("Compliments! You are entitled for the \"Final Destination\" discount! -40%.");
-                eurosPerKm -= eurosPerKm * .40;
-            } else {
-                this.#say("Alright, full price my friend, but bring your family next time for huge discounts!");
-            }
-
-            // To the math...
-            let price = eurosPerKm * form.km;
-            let prefix;
-            if (this.#mood == "BAD") {
-                prefix = "[grunting] ";
-                price = Math.ceil(price);
-            } else {
-                prefix = "[smiling] ";
-                price = Math.floor(price);
-            }
-
-            // Generate ticket
-            let ticket = {
-                price: `${price}€`,
-            }
-
-            this.#say(`${prefix}Your ticket is ${ticket.price}.`)
-            return ticket;
-
-        } else {
-            // Player is empty handed
-            this.#say("Well, first you'll have to fill this form");
-            this.#say(
-                `Come back later and ask again 
-                with the compiled form`
-                );
-
-            const FORM = {
-                age: "fill here",
-                km: "fill here"
-            };
-
-            // Just in case you loose it ;)
-            setTimeout(() => {
-                this.#say("If you loose your FORM you may find a new one in the DRAWER");
-                window.DRAWER = {
-                    "FORM": FORM,
-                }
-            }, 5000);
-
-            return FORM;
-        }
-    }
-}
-
-// Create RailwayMan
-let RailMan = new RailwayMan("Mike");
